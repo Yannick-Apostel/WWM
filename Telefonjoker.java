401197414c0dbd2bca1081eed892a703bdc13689
Constantin <constantin.p2207@gmail.com>
2023-10-19 12:22:53 +0200

    implement telefonjoker

--- Telefonjoker.java
+++ Telefonjoker.java
@@ -1,3 +1,28 @@
+import java.util.*;
 public class Telefonjoker {
+    ArrayList<String> Kontakte = new ArrayList<String>();
+    public Telefonjoker() {
+        super();
+        
+        
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Wer sind ihre drei Kontakte?");
+        for(int i=0;i<3;i++){
+        String userKontakt = scan.nextLine();
+        Kontakte.add(userKontakt);
+        }
+
+        
+    }
     
+    public String auswahl(String wahl){
+            for(String Kontakt:Kontakte){
+                if(Kontakt.equals(wahl)){
+                    return Kontakt;
+                }
+            }
+            return "Kontakt nicht vorhanden";
+            
+        }
 }
