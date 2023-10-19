d728150e2ae3769f81e69e2631978dc91f23f0e6
Yannick Apostel <67263923+Yannick-Apostel@users.noreply.github.com>
2023-10-19 12:43:23 +0200

    added Publikumsjoker

--- Publikumsjoker.java
+++ Publikumsjoker.java
@@ -1,5 +1,18 @@
 public class Publikumsjoker {
-   public Publikumsjoker() {
-    System.out.println("Hallo");
+   
+   
+   public  Antwort AntwortPublikumsjoker(Antwort a1, Antwort a2, Antwort a3, Antwort a4) {
+     Services zufallsgen = new Services();
+
+     int zufallzahl = zufallsgen.erzeugeZufallszahl(1, 4);
+
+     switch(zufallzahl){
+      case 1: return a1;
+      case 2: return a2;
+      case 3: return a3; 
+      case 4: return a4;
+     }
+
+     return null;
    }
 }
