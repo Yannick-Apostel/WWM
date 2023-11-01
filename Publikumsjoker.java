681eb90543e5dd87c191061792f081f98a9f08e6
Yannick Apostel <67263923+Yannick-Apostel@users.noreply.github.com>
2023-11-01 09:36:23 +0100

    implementation of Telefonjoker

--- Publikumsjoker.java
+++ Publikumsjoker.java
@@ -1,19 +1,21 @@
 public class Publikumsjoker {
-   
-   
-   public  Antwort AntwortPublikumsjoker(Antwort a1, Antwort a2, Antwort a3, Antwort a4) {
-     Services zufallsgen = new Services();
 
-     int zufallzahl = zufallsgen.erzeugeZufallszahl(1, 4);
+  public Antwort AntwortPublikumsjoker(Antwort a1, Antwort a2, Antwort a3, Antwort a4) {
+    Services zufallsgen = new Services();
 
-     switch(zufallzahl){
-      case 1: System.out.println("A1");
-       return a1;
-      case 2:System.out.println("A2"); return a2;
-      case 3: System.out.println("A3"); return a3; 
-      case 4: System.out.println("A4"); return a4;
-     }
+    int zufallzahl = zufallsgen.erzeugeZufallszahl(1, 4);
 
-     return null;
-   }
+    switch (zufallzahl) {
+      case 1:
+        return a1;
+      case 2:
+        return a2;
+      case 3:
+        return a3;
+      case 4:
+        return a4;
+    }
+
+    return null;
+  }
 }
